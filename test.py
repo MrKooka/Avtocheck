1f8f5ba406d2a0d4907edba98b8ec4cf392eb978
Avtocheck <kooka42@yandex.ru>
2021-01-29 14:12:00 +0400

    Test commit

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from models import Avto
 a = Avto(name='lol',price=1212,transmission='sda',drive_unit='sadasd',engen='sdas',type_engen='sadasd',url='asd',city='asd',year=123)
 db.session.add(a)
 db.session.commit()
+print('lol')
