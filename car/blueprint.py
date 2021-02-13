9e3370827d8c60e425c5b50e9d7f25fd7025e4c5
Kooka <kukeev1999@gmail.com>
2021-02-13 13:17:15 +0400

    Сделал стили для форм регистрации и формы войти

--- car/blueprint.py
+++ car/blueprint.py
@@ -1,4 +1,4 @@
-from flask import render_template,Blueprint,redirect,url_for,request,jsonify,current_app
+from flask import render_template,Blueprint,redirect,url_for,request,jsonify,current_app,flash
 from flask_login import login_user,logout_user,current_user
 #декоратор который скрывает обработчики от пользователей с неподходищими ролями
 from flask_security import login_required
@@ -43,8 +43,16 @@ def choice_car(id_car):
 		city = form.cities.data
 		comment = form.comment.data
 		id_car = request.form['value_id_car']
-		data = RequestForm(phone=int(phone),name=name,email=email,city=city,
-						   comment=comment,id_car=id_car)
+
+		try:
+
+			
+			data = RequestForm(phone=int(phone),name=name,email=email,city=city,
+							   comment=comment,id_car=id_car)
+			flash('Форма отпрвлена')
+		except:
+			flash('Не все поля формы заполнены корректно')
+			return redirect(url_for('cars.choice_car',id_car=id_car))
 		db.session.add(data)
 		db.session.commit()
 		return redirect(url_for('cars.index'))
@@ -59,9 +67,10 @@ def choice_car(id_car):
 @cars.route('/signup', methods=['POST','GET'])
 def signup():
 	form = RegisterForm_()
-	print(request.form)
-	if request.method=='POST':
+	if form.validate_on_submit():
+
 		if request.form['b'] == 'a':
+			# try:
 			hashed_pass = generate_password_hash(form.password.data,method='sha256')
 			new_user = user_datastore.create_user(email=form.email.data,
 												  password=hashed_pass,
@@ -73,28 +82,36 @@ def signup():
 			db.session.commit()
 			user_datastore.add_role_to_user(new_user,role)
 			db.session.commit()
+			# except:
+			# 	print('Ошибка при доьавлении в базу ')
+			# 	redirect(url_for('cars.signup'))
 			return redirect(url_for('.index'))
+		return redirect(url_for('cars.signup'))
 		if request.form['b'] == 'b':
-			hashed_pass = generate_password_hash(form.password.data,method='sha256')
-			new_user = user_datastore.create_user(email=form.email.data,
-												  password=hashed_pass,
-												  phone=int(form.phone.data),
-												  username=form.username.data)
-			role = Role(name=form.username.data,discription='user')
-			db.session.add(new_user)
-			db.session.add(role)
-			db.session.commit()
-			user_datastore.add_role_to_user(new_user,role)
-			db.session.commit()
+			try:
+				hashed_pass = generate_password_hash(form.password.data,method='sha256')
+				new_user = user_datastore.create_user(email=form.email.data,
+													  password=hashed_pass,
+													  phone=int(form.phone.data),
+													  username=form.username.data)
+				role = Role(name=form.username.data,discription='user')
+				db.session.add(new_user)
+				db.session.add(role)
+				db.session.commit()
+				user_datastore.add_role_to_user(new_user,role)
+				db.session.commit()
+			except:
+				redirect(url_for('cars.signup'))
 			return redirect(url_for('.index'))
-
+		return redirect(url_for('cars.signup'))
 	return render_template('cars/signup.html',form=form)
 
 
 @cars.route('/login',methods=['POST','GET'])
 def login():
+	print(current_user.__dict__)
 	form = LoginForm()
-	if request.method == 'POST':
+	if form.validate_on_submit():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user:
 			if check_password_hash(user.password, form.password.data):
@@ -113,7 +130,7 @@ def profil():
 	return render_template('cars/profil.html',role=role,current_user=current_user,Avto=Avto,\
 							form_and_car_id=form_and_car_id,RequestForm=RequestForm,\
 							)
-
+	
 
 
 @cars.route('/logout')
@@ -134,3 +151,16 @@ def logout():
 # 		cityArray.append(cityObj)
 # 	return jsonify({'cities':cityArray})
 
+# @cars.route('/signup', methods=['POST','GET'])
+# def test():
+# 	form = RegisterForm_()
+# 	print(form.email.errors)
+# 	# [print(i) for i in dir(form)]
+# 	if form.validate_on_submit():
+# 		request.form['b']
+
+
+		
+# 	else:
+# 		print('Не POST')
+# 	return render_template('cars/signup.html',form=form)
--- car/form.py
+++ car/form.py
@@ -5,49 +5,42 @@ from wtforms.validators import InputRequired,Email,Length,DataRequired
 from wtforms.fields.html5 import SearchField
 import sys
 import phonenumbers
+ 
 sys.path.insert(0,'/media/alex/Data1/two/Avtocheck')
 # from app import session,Cities
 
 class RequestForm_(FlaskForm):
-	email    = StringField("Email:",validators=[Email()])
-	phone    = IntegerField('Телефон:')
-	name     = StringField('Ваше имя:')
-	cities   = SelectField('Ваш город',choices=[])
-	comment  = TextAreaField('Комментарий')
-	submit   = SubmitField("Отправить заявку")
-
-# class RegistrationForm(FlaskForm):
-# 	email = SubmitField('Email:',validators=[Email()])
-# 	phone = IntegerField('Телефон:')
-# 	name = StringField('Ваше имя:')
-# 	psw = PasswordField()
-
-
-class LoginForm(FlaskForm):
-    Email    = StringField('Email', validators=[Email()])
-    username = StringField('Ваше имя',validators=[InputRequired(),Length(min=4,max=15)])
-    password = PasswordField('Пароль', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('Запомните меня')
-    phone    = IntegerField('Телефон')
-    submit   = SubmitField("Войти")
+	email = StringField("Email:",validators=[Email('Некорректный email')])
+	phone = IntegerField('Телефон:')
+	name = StringField('Ваше имя:')
+	cities = SelectField('Ваш город',choices=[])
+	comment = TextAreaField('Комментарий')
+	submit = SubmitField("Отправить заявку")
 
 class RegisterForm_(FlaskForm):
-    phone    = IntegerField('Телефон',validators=[InputRequired(message='Поеле не заполнено')])
-    email    = StringField('Email', validators=[InputRequired(), Email(message='Invalid email')])
-    username = StringField('Ваше имя')
-    password = PasswordField('password', validators=[InputRequired(message='Поеле не заполнено'), Length(min=8, max=80)])
-    submit   = SubmitField('Зарегистрироваться')
-    # def validate_phone(form, field):
-    #     if 10 > len(field.data) > 12:
-    #         raise ValidationError('Invalid phone number.')
-    #     try:
-    #         input_number = phonenumbers.parse(field.data)
-    #         if not (phonenumbers.is_valid_number(input_number)):
-    #             raise ValidationError('Invalid phone number.')
-    #     except:
-    #         input_number = phonenumbers.parse("+1"+field.data)
-    #         if not (phonenumbers.is_valid_number(input_number)):
-    #             raise ValidationError('Invalid phone number.')
+    email = StringField('Email:',validators=[Email('некорректный email')])
+    phone = IntegerField('Телефон:')
+    username = StringField('Ваше имя', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Пароль',validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Зарегистрироваться')
+# class RegisterForm_(FlaskForm):
+#     phone = IntegerField('Телефон',validators=[InputRequired(message='Поеле не заполнено')])
+#     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email')])
+#     username = StringField('Ваше имя')
+#     password = PasswordField('password', validators=[InputRequired(message='Поеле не заполнено'), Length(min=8, max=80)])
+#     submit = SubmitField('Зарегистрироваться')
+
+#     def validate_phone(form, field):
+#         if 10 > len(field.data) > 12:
+#             raise ValidationError('Invalid phone number.')
+#         try:
+#             input_number = phonenumbers.parse(field.data)
+#             if not (phonenumbers.is_valid_number(input_number)):
+#                 raise ValidationError('Invalid phone number.')
+#         except:
+#             input_number = phonenumbers.parse("+1"+field.data)
+#             if not (phonenumbers.is_valid_number(input_number)):
+#                 raise ValidationError('Invalid phone number.')
 # class RegisterForm(FlaskForm):
 #     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
 #     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -55,7 +48,7 @@ class RegisterForm_(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email    = StringField('Email', validators=[InputRequired(),Email()])
+    email = StringField('Email', validators=[InputRequired(),Email()])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
-    submit   = SubmitField('Войти')
+    submit = SubmitField('Войти')
